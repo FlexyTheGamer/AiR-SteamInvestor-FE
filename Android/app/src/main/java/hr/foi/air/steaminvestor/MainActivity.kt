0ceb6294dac0e7b3836c63de8018022f338ef323
Ivan Giljević <igiljevic@student.foi.hr>
2024-10-29 20:43:02 +0100

    Inicijalizacija FE projekta
    Sadrži:
    -Generirane datoteke prilikom kreacije novog projekta u AndroidStudio

---
+++ Android/app/src/main/java/hr/foi/air/steaminvestor/MainActivity.kt
@@ -0,0 +1,47 @@
+package hr.foi.air.steaminvestor
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import hr.foi.air.steaminvestor.ui.theme.SteamInvestorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SteamInvestorTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Steam User",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SteamInvestorTheme {
+        Greeting("Android")
+    }
+}
